ffe7b06685adf0a00cd47e6468513fa10d5a33b3
Symbionte <joevecnasorb@gmail.com>
2023-10-16 12:09:26 -0300

    update Ingreso de eventos

---
+++ src/controller/gestorEventosIngreso.js
@@ -0,0 +1,43 @@
+// Variable global donde se cargan los eventos
+var eventos = [];
+
+function cargarEventos() {
+    // Se cargan eventos previamente cargados
+    // Intentamos cargar el archivo JSON existente (si existe)
+    fetch('../model/registroEventos.json')
+        .then(response => response.json())
+        .then(data => {
+            eventos = data;
+        })
+        .catch(error => console.error("No se pudo cargar el archivo JSON", error));
+}
+
+function agregarEvento(){
+    // Agregamos el nuevo evento
+    var evento = {
+        nombre: document.getElementById("nombre").value,
+        lugar: document.getElementById("lugar").value,
+        horario: document.getElementById("horario").value,
+        descripcion: document.getElementById("descripcion").value
+    };
+
+    eventos.push(evento);
+
+    // Vaciamos los campos del formulario
+    document.getElementById("nombre").value = "";
+    document.getElementById("lugar").value = "";
+    document.getElementById("horario").value = "";
+    document.getElementById("descripcion").value = "";
+}
+
+function descargarEventos() {
+    // Guardamos los datos actualizados en el archivo JSON
+    const jsonStr = JSON.stringify(eventos);
+    
+    const blob = new Blob([jsonStr], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "registroEventos.json";
+    a.click();
+}
