328dc09c0776406aa7106af52720bcdb9dac3d37
Alan <alanechabarri@gmail.com>
2023-10-20 15:47:15 -0300

    #12,#14,RF14,RF13 completadas.

--- src/controller/gestorArticulosyservicios.js
+++ src/controller/gestorArticulosyservicios.js
@@ -1,20 +1,41 @@
 
-function agregarDatosAlJSON() {
-    const formData = {
-        categoria: document.getElementById("categoria").value,
-        title: document.getElementById("fname").value,
-        tipo: document.getElementById("tipo").value,
-        descripcion: document.querySelector('textarea[name="message"]').value,
-        precio: document.getElementById("precio").value,
-        imagen: document.getElementById("myPic").value,
-        restricciones: {
-            niños: document.getElementById("niños").checked,
-            adultos: document.getElementById("adultos").checked,
-            movilidad: document.getElementById("movilidad").checked,
-            otro: document.getElementById("rOtro").value
-        }
-    };
-
+function agregarDatosAlJSON(validado) {
+    if (validado) {
+        var fechaActual = new Date();
+        var dia = fechaActual.getDate();
+        var mes = fechaActual.getMonth() + 1;
+        var año = fechaActual.getFullYear();
+        const formData = {
+            fecha: dia + '/' + mes + '/' + año,
+            categoria: document.forms["publisher"].elements["categoria"].value,
+            title: document.forms["publisher"].elements["title"].value,
+            disponible: true,
+            tipo: document.forms["publisher"].elements["tipo"].value,
+            resumen: document.forms["publisher"].elements["message"].value,
+            precio: document.forms["publisher"].elements["precio"].value,
+            imagen: {
+                img1: document.forms["publisher"].elements["myPic"].value,
+                img2: document.forms["publisher"].elements["myPic2"].value,
+                img3: document.forms["publisher"].elements["myPic3"].value,
+                img4: document.forms["publisher"].elements["myPic4"].value,
+                img5: document.forms["publisher"].elements["myPic5"].value
+            },
+            restricciones: {
+                niños: document.forms["publisher"].elements["niños"].checked,
+                adultos: document.forms["publisher"].elements["adultos"].checked,
+                movilidad: document.forms["publisher"].elements["movilidad"].checked,
+                otro: document.forms["publisher"].elements["rOtro"].value
+            },
+            descripcion: document.forms["publisher"].elements["descripcion"].value,
+            dimension: document.forms["publisher"].elements["dimensiones"].value,
+            materiales: document.forms["publisher"].elements["materiales"].value,
+            colores: document.forms["publisher"].elements["colores"].value,
+            contacto: document.forms["publisher"].elements["contacto"].value,
+            ubicacion: {
+                latitud: document.forms["publisher"].elements["latitud"].value,
+                longitud: document.forms["publisher"].elements["longitud"].value
+            }
+        };
     // Intentamos cargar el archivo JSON existente (si existe)
     let jsonData = [];
 
@@ -39,4 +60,29 @@ function agregarDatosAlJSON() {
             a.download = "productos.json";
             a.click();
         });
+}
+else{alert("No se ha podido validar los datos correctamente,compruebe todos los campos obligatorios e  intente nuevamente")
+    
+}}
+
+function cargarProducto() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "../model/productos.json", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var productos = JSON.parse(xhr.responseText);
+            var tarjetas = document.querySelectorAll(".card-body");
+
+            for (var i = 0; i < eventos.length; i++) {
+                tarjetas[i].querySelector(".titulo").textContent = productos[i]["Titulo del evento"];
+                tarjetas[i].querySelector(".lugar").textContent = productos[i]["Lugar"];
+                tarjetas[i].querySelector(".horario").textContent = productos[i]["Horario"];
+                tarjetas[i].querySelector(".entrada").textContent = productos[i]["Entrada"];
+                tarjetas[i].querySelector(".descripcion").textContent = productos[i]["Descripcion"];
+                /*tarjetas[i].querySelector(".latitud").textContent = eventos[i]["Ubicacion"]["latitud"];
+                tarjetas[i].querySelector(".longitud").textContent = eventos[i]["Ubicacion"]["longitud"];
+            */ }
+        }
+    };
+    xhr.send();
 }
--- src/view/comerciante.js
+++ src/view/comerciante.js
@@ -1,11 +1,78 @@
+function continuar(actual, siguiente) {
+    if (validar(actual)) {
+        mostrarElemento(siguiente);
+        ocultarElemento(actual);
+    }
+    else{
+        alert("un campo no se lleno")
+    }
 
+}
 // Manejar la navegación entre pasos
-function mostrarElemento(id){
+function mostrarElemento(id) {
+
     document.getElementById(id).style.display = "flex";
- 
+    setTimeout(function () {
+        var miDiv = document.getElementById(id); // Reemplaza 'miDiv' con el ID de tu div
+        miDiv.classList.add('aparecer', 'mostrar');
+        // Tu código que deseas ejecutar después de 1 segundo
+    }, 50);
+
+
 }
-function ocultarElemento(id){
+function ocultarElemento(id) {
     document.getElementById(id).style.display = "none";
-    
+    var elemento = document.getElementById(id); // Reemplaza "miElemento" con el ID o selector de tu elemento
+
+    // Quita la clase "mostrar" del elemento
+    elemento.classList.remove("mostrar");
+
+
 }
-console.log("hola sss")
+function validar(id) {
+    // Encuentra el elemento con el id "step-2"
+    var seccion = document.getElementById(id);
+
+    // Busca los campos "required" solo dentro de la sección "step-2"
+    var camposRequeridos = seccion.querySelectorAll('[required]');
+    var todosLlenos = true;
+
+    camposRequeridos.forEach(function (element) {
+        if (element.value.trim() === '') {
+            todosLlenos = false;
+        }
+    });
+    return todosLlenos
+
+}
+function articulosSugeridosDe(articulo) {
+
+    var sug = document.getElementById('sugerencia');
+    document.getElementById('sugerencia').style.display = "block";
+    sug.textContent=articulo;
+
+    // Realiza la solicitud a la API de Mercado Libre utilizando fetch
+    fetch('https://api.mercadolibre.com/sites/MLA/search?q=' + articulo)
+        .then(response => response.json())
+        .then(data => {
+            // Obtiene el primer resultado de la API (primer producto)
+            const primerResultado = data.results[0];
+        
+            
+           
+            var lista = document.createElement('ul'); // Crear una lista ul
+            
+            for (i = 0; i < 10; i++) {
+                var item = document.createElement('li'); // Crear elementos li
+                item.textContent = data.results[i].title+" Precio:"+data.results[i].price+" Ars";
+                lista.appendChild(item); // Agregar el elemento li a la lista ul
+            }
+            
+            sug.appendChild(lista); // Agregar la lista al elemento con el id 'sugerencia'
+            
+            console.log("el precio es" + primerResultado.price + "fin precio");
+        })
+        .catch(error => {
+            console.error('Error al hacer la solicitud:', error);
+        });
+}
